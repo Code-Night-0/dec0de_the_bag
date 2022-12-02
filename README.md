58ca9bb4c75c1f4ea0cfe26e38f05a5f45361936
berkay yıldız <37070272+berkaayildiz@users.noreply.github.com>
2022-12-02 05:26:24 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# story
+
+After a long night, a successful robbery had taken place. Or was it? The character that the player is in dialogue with ,which is the person who carried out the robbery, what he got in the heist was nothing but the characters he thought were random. The robber meets the player after the robbery and the game starts here. The player's objective is to interact with the game to find the valuable thing hidden behind cryption and help the robber.
+
+### how to play?
+
+1. First you need python installed on your computer.
+2. Download the dec0de_the_bag.py file.
+3. Open the terminal.
+4. After that type this in your terminal.
+```
+python3 "your\path\of\downloaded\file"
+```
+5. D0ne! The game should run perfectly.
---
+++ decode_the_bag.py
@@ -0,0 +1,88 @@
+import time
+
+base64 = "MElaN0lM"
+pasteit = "0IZ7IL"
+value = "0xa2173120xe193244193v39481394x3984391e349"
+
+hint1 = ">> ugh, americans, i don't know something about 'has just 128 ... only 95 are...'"
+hint2 = ">> i would help you But i hAve no SinglE clue a6out wh4t does it..."
+
+print(">>> shit... wHy does it have to be like this! what kind of money is this?!")
+time.sleep(3.5)
+print(">>> is the government really protectIng something like this in its bank?!")
+time.sleep(3.5)
+
+print(">>> oh hello boy, what is your Name?")
+time.sleep(1.5)
+player = input("$$$ ")
+
+print(">>> ", player, "... whaTever... can you help me about this?")
+time.sleep(2.5)
+print(">>> actually, this wasn't a question, you are helping me.")
+time.sleep(2.5)
+print(">>> here is the thing i stole, now do your thing!")
+time.sleep(2.5)
+
+print("""
+         ┌────────────────────────────────────────────────────┐
+         │                                                    │
+         │           077 069 108 097 078 048 108 077          │
+         │                                                    │
+         └────────────────────────────────────────────────────┘
+    """)
+
+answer = input("$$$ ")
+
+while answer != base64:
+    if answer == "HINT":
+        print(hint1)
+        answer = input("$$$ ")
+    else:
+        print("access declined. try again.")
+        answer = input("$$$ ")
+
+print(">>> nice big boy! i feel like we are getting closer. just decode this and i think we are done.")
+time.sleep(3.5)
+
+answer = input("$$$ ")
+
+while answer != pasteit:
+    if answer == "HINT":
+        print(hint2)
+        answer = input("$$$ ")
+    else:
+        print("access declined. try again.")
+        answer = input("$$$ ")
+
+print(">>> wow... to be honest, i had no faith in you, but you proved me wrong.")
+time.sleep(3.5)
+print(">>> actually i was testing you, here is the paper i found while stoling this: ")
+time.sleep(3.5)
+
+print("""
+         ┌────────────────────────────────────────────────────┐
+         │                                                    │
+         │        aHR0cHM6Ly93d3cuaGV5cGFzdGVpdC5jb20=        │
+         │                                                    │
+         └────────────────────────────────────────────────────┘
+    """)
+
+answer = input("$$$ ")
+
+while answer != value:
+    if answer == "HINT":
+        print(">>> dumbass, no hint for this one.")
+        answer = input("$$$ ")
+    else:
+        print(">>> it declined! don't play with me and find it right now!")
+        answer = input("$$$ ")
+
+print(">>> haha thanks! IT WORKED!")
+time.sleep(3.5)
+print("...")
+time.sleep(3.5)
+print("...")
+time.sleep(3.5)
+print(">>> IDIOT")
+time.sleep(1.5)
+print(">>> Bye.")
